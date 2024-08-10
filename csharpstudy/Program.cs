97149b106556e2870d36e676e4ccc3590fca456f
tmdals12 <hahasm3136@gmail.com>
2024-08-10 15:09:30 +0900

    첫 커밍

---
+++ csharpstudy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharpstudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
